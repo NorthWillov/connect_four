9e8a3cbb1dd069baf512363afc3f8fe1849cc2fe
NorthWillov <northwillov@gmail.com>
2021-11-19 16:36:47 +0100

    Add event listener to Box

--- src/Box.tsx
+++ src/Box.tsx
@@ -1,7 +1,18 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 
 const Box: FC = () => {
-  return <div className="Box"></div>
+  const [isClicked, setIsClicked] = useState(false)
+
+  const hanldeClick = () => {
+    setIsClicked(true)
+  }
+
+  return (
+    <div
+      className={`Box ${isClicked ? "Box-Red" : null}`}
+      onClick={hanldeClick}
+    ></div>
+  )
 }
 
 export default Box
